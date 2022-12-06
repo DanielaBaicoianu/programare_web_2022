269725174a2b48abae4066e7a874926e4e5e19ee
DanielaBaicoianu <114957081+DanielaBaicoianu@users.noreply.github.com>
2022-12-06 17:20:11 +0200

    Add files via upload

---
+++ Projects/Seminar9/Seminar9/src/main/java/ex1/entity/User.java
@@ -0,0 +1,43 @@
+package ex1.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "user_type")
+    private String userType;
+
+}
