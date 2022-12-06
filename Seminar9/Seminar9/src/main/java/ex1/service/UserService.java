1aa29970e16b03091e0b97046bacfb44437f12f7
DanielaBaicoianu <114957081+DanielaBaicoianu@users.noreply.github.com>
2022-12-06 18:07:45 +0200

    Add files via upload

---
+++ Seminar9/Seminar9/src/main/java/ex1/service/UserService.java
@@ -0,0 +1,66 @@
+package ex1.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ex1.dto.UserDto;
+import ex1.entity.User;
+import ex1.exception.UserNotFoundException;
+import ex1.mapper.UserMapper;
+import ex1.repository.UserRepository;
+
+@Service
+public class UserService {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public UserDto addUser(UserDto userDto) {
+        return userMapper.mapToUserDto(userRepository.save(userMapper.mapToUser(userDto)));
+    }
+
+    public List<UserDto> getByUsername(String username) {
+        List<UserDto> userDtos = userRepository.findByUsername(username).stream()
+                .map(user -> userMapper.mapToUserDto(user)).collect(Collectors.toList());
+        if (userDtos.isEmpty()) {
+            throw new UserNotFoundException(String.format("No user %s found on the db", username));
+        }
+        return userDtos;
+    }
+
+    public List<UserDto> getByUsernameAndType(String username, String type) {
+        List<UserDto> userDtos = userRepository.filterByType(username, type).stream()
+                .map(user -> userMapper.mapToUserDto(user)).collect(Collectors.toList());
+        if (userDtos.isEmpty()) {
+            throw new UserNotFoundException(String.format("No user %s found on the db", username));
+        }
+        return userDtos;
+
+    }
+
+    public UserDto findFirstContaining(String fullName) {
+        User user = userRepository.findFirstByFullNameContaining(fullName);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("No user containing %s in full name found on the db", fullName));
+        }
+        return userMapper.mapToUserDto(user);
+    }
+
+    public UserDto updateUsername(Long id, String username) {
+
+        User user = userRepository.getReferenceById(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("No user with id %s found", id));
+        }
+        user.setUsername(username);
+        return userMapper.mapToUserDto(userRepository.save(user));
+    }
+}
